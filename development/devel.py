3153399affca0042b349235e2c9cc6d8f2bb35b4
Kevin <kevin.sawade@uni-konstanz.de>
2021-07-22 11:16:53 +0200

    Commit of some changes.

---
+++ development/devel.py
@@ -0,0 +1,9 @@
+# %% Imports
+import xplor
+
+# %% make the tbl files
+# xplor.functions.make_15_N_table('data/spre_and_relaxation_data_k6_k29/relaxation_file_ub2_k6.txt', out_file='/home/kevin/git/xplor_functions/xplor/data/diUbi_sPRE_k6_w_CONECT.tbl')
+xplor.functions.write_argparse_lines_from_yaml()
+
+# %%
+xplor.functions.call_xplor_with_yaml('test.pdb', from_tmp=True)
