ce636b56341ddde850c92ef1e56fd06bb1ed2b55
Kevin Sawade <kevin.sawade@uni-konstanz.de>
2021-08-14 16:34:34 +0200

    Added a script to run xplor.functions.parallel_xplor with nohup

---
+++ xplor/scripts/nohup_xplor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import xplor
+
+xplor.functions.parallel_xplor(['k6', 'k29', 'k33'], df_outdir='/home/kevin/projects/tobias_schneider/values_from_every_frame/from_package_w_conect/', suffix='_df.csv')
+
