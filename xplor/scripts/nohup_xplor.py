b64a112e23bb59dbe5e309e8c1cf84978a25c51d
Kevin Sawade <kevin.sawade@uni-konstanz.de>
2021-08-16 00:15:35 +0200

    Added parallel=False to nohup_xplor.py

--- xplor/scripts/nohup_xplor.py
+++ xplor/scripts/nohup_xplor.py
@@ -2,4 +2,4 @@
 
 import xplor
 
-xplor.functions.parallel_xplor(['k6', 'k11', 'k33'], df_outdir='/home/kevin/projects/tobias_schneider/values_from_every_frame/from_package_with_conect/', suffix='_df.csv')
+xplor.functions.parallel_xplor(['k6', 'k11', 'k33'], df_outdir='/home/kevin/projects/tobias_schneider/values_from_every_frame/from_package_with_conect/', suffix='_df.csv', parallel=False)
